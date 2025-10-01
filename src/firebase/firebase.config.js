e90f6f140596116f60a0c5037373cf09c5571c15
Md belayet <mdbelayethossain016@gmail.com>
2025-10-01 13:07:26 +0600

    created signIn and signUp component

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIbg74Soi3doycO4kXxTToDYX0ZtGNNSI",
+  authDomain: "coffee-client-2398d.firebaseapp.com",
+  projectId: "coffee-client-2398d",
+  storageBucket: "coffee-client-2398d.firebasestorage.app",
+  messagingSenderId: "918611025495",
+  appId: "1:918611025495:web:200ec9c66e194759028d12"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import AddCoffee from './components/AddCoffee.jsx';
 import UpdateCoffee from './components/UpdateCoffee.jsx';
+import SignIn from './components/SignIn.jsx';
+import SignUp from './components/SignUp.jsx';
 
 const router = createBrowserRouter([
   {
@@ -21,6 +23,14 @@ const router = createBrowserRouter([
     path: "/updateCoffee/:id",
     element: <UpdateCoffee></UpdateCoffee>,
     loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
+  },
+  {
+    path: "/signIn",
+    element: <SignIn></SignIn>
+  },
+  {
+    path: "/signUp",
+    element: <SignUp></SignUp>
   },
 ]);
 
